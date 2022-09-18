0a9a01f219838985ac9f18d3cf3431c0d556297a
Om Joshi <omjoshi003@gmail.com>
2022-09-18 15:15:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical3_20012021010/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.madpractical3_20012021010
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.madpractical3_20012021010.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.BottomNavigationView.id = R.id.bottom_nav_reg
+        binding.BottomNavigationView.setOnItemSelectedListener { it2 ->
+            when(it2.itemId) {
+                R.id.bottom_nav_reg -> {
+                    Intent(this, RegistrationActivity::class.java).also { startActivity(it) }
+                }
+                R.id.bottom_nav_login -> {
+                    Intent(this, LoginActivity::class.java).also { startActivity(it) }
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+}
